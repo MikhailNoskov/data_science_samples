a956f587fb870fe2e1465acbdca84a63c2a76922
Михаил Носков <rjd@yandex.ru>
2023-12-27 22:37:33 +0100

    digit images detection

--- 4_1_sklearn_intro.py
+++ 4_1_sklearn_intro.py
@@ -9,62 +9,125 @@ from sklearn.decomposition import PCA
 
 # Iris sample
 #
-iris = sns.load_dataset('iris')
-# print(iris.head())
-# # sns.pairplot(iris, hue='species', height=1.5)
+# iris = sns.load_dataset('iris')
+# # print(iris.head())
+# # # sns.pairplot(iris, hue='species', height=1.5)
+# # # plt.show()
+# X_iris = iris.drop('species', axis=1)
+# # print(X_iris.shape)
+# y_iris = iris['species']
+# # print(y_iris.shape)
+#
+#
+# # Supervised Learning Example: Simple Linear Regression
+#
+# # rng = np.random.RandomState(42)
+# # x = 10 * rng.rand(50)
+# # y = 2 * x - 1 + rng.randn(50)
+# # plt.scatter(x, y)
 # # plt.show()
-X_iris = iris.drop('species', axis=1)
-# print(X_iris.shape)
-y_iris = iris['species']
-# print(y_iris.shape)
-
-
-# Supervised Learning Example: Simple Linear Regression
-
-# rng = np.random.RandomState(42)
-# x = 10 * rng.rand(50)
-# y = 2 * x - 1 + rng.randn(50)
-# plt.scatter(x, y)
+# # model = LinearRegression(fit_intercept=True)
+# # X = x[:, np.newaxis]
+# # model.fit(X, y)
+# # xfit = np.linspace(-1, 11)
+# # Xfit = xfit[:, np.newaxis]
+# #
+# # yfit = model.predict(Xfit)
+# #
+# # plt.scatter(x, y)
+# # plt.scatter(xfit, yfit, cmap='Red')
+# # plt.plot(xfit, yfit)
+#
+# # Supervised Learning Example: Iris Classication
+#
+# # Xtrain, Xtest, ytrain, ytest = train_test_split(X_iris, y_iris, random_state=1)
+# # model = GaussianNB()  # 2. instantiate model
+# # model.fit(Xtrain, ytrain)  # 3. fit model to data
+# # y_model = model.predict(Xtest)  # 4. predict on new data
+# #
+# # print(accuracy_score(ytest, y_model))
+#
+# # Unsupervised Learning Example: Iris Dimensionality
+#
+# model = PCA(n_components=2)  # 2. instantiate model
+# model.fit(X_iris)  # 3. fit model to data
+# X_2D = model.transform(X_iris)  # 4. transform the data
+#
+# iris['PCA1'] = X_2D[:, 0]
+# iris['PCA2'] = X_2D[:, 1]
+#
+# # sns.lmplot(x="PCA1", y="PCA2", hue='species', data=iris, fit_reg=False)
+# #
+# # # Unsupervised Learning Example: Iris Clustering
+# #
+# from sklearn.mixture import GaussianMixture
+#
+# model = GaussianMixture(n_components=3, covariance_type='full')  # 2. instantiate model
+# model.fit(X_iris)  # 3. fit model to data
+# y_gmm = model.predict(X_iris)  # 4. determine labels
+# iris['cluster'] = y_gmm
+# sns.lmplot(x="PCA1", y="PCA2", data=iris, hue='species',col='cluster', fit_reg=False)
 # plt.show()
-# model = LinearRegression(fit_intercept=True)
-# X = x[:, np.newaxis]
-# model.fit(X, y)
-# xfit = np.linspace(-1, 11)
-# Xfit = xfit[:, np.newaxis]
-#
-# yfit = model.predict(Xfit)
-#
-# plt.scatter(x, y)
-# plt.scatter(xfit, yfit, cmap='Red')
-# plt.plot(xfit, yfit)
 
-# Supervised Learning Example: Iris Classication
+# Application: Exploring Handwritten Digits
 
-# Xtrain, Xtest, ytrain, ytest = train_test_split(X_iris, y_iris, random_state=1)
-# model = GaussianNB()  # 2. instantiate model
-# model.fit(Xtrain, ytrain)  # 3. fit model to data
-# y_model = model.predict(Xtest)  # 4. predict on new data
+from sklearn.datasets import load_digits
+
+digits = load_digits()
+# print(digits.images.shape)
+
+fig, axes = plt.subplots(10, 10, figsize=(8, 8),
+                         subplot_kw={'xticks': [], 'yticks': []},
+                         gridspec_kw=dict(hspace=0.1, wspace=0.1))
+
+# for i, ax in enumerate(axes.flat):
+#     ax.imshow(digits.images[i], cmap='binary', interpolation='nearest')
+#     ax.text(0.05, 0.05, str(digits.target[i]), transform=ax.transAxes, color='green')
+
+X = digits.data   # Samples as arrays of 64 pixels (features)
+y = digits.target  # Target data
+
+# print(X)
+# print(y)
+
+# Unsupervised Learning Example: Dimensionality Reduction
+
+# from sklearn.manifold import Isomap
 #
+# iso = Isomap(n_components=2)  # We change form 64 features to 2
+# iso.fit(digits.data)
+# data_projected = iso.transform(digits.data)
+# print(data_projected.shape)
+#
+# plt.scatter(data_projected[:, 0], data_projected[:, 1], c=digits.target,
+#             edgecolor='none', alpha=0.5,
+#             cmap=plt.cm.get_cmap('viridis', 10))
+# plt.colorbar(label='digit label', ticks=range(10))
+# plt.clim(-0.5, 9.5)
+
+# Classication on Digits
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, random_state=0)
+from sklearn.naive_bayes import GaussianNB
+
+model = GaussianNB()
+model.fit(Xtrain, ytrain)
+y_model = model.predict(Xtest)
+
+from sklearn.metrics import accuracy_score
+
 # print(accuracy_score(ytest, y_model))
+from sklearn.metrics import confusion_matrix
 
-# Unsupervised Learning Example: Iris Dimensionality
+# mat = confusion_matrix(ytest, y_model)
+# sns.heatmap(mat, square=True, annot=True, cbar=False, cmap='Blues')
+# plt.xlabel('predicted value')
+# plt.ylabel('true value')
 
-model = PCA(n_components=2)  # 2. instantiate model
-model.fit(X_iris)  # 3. fit model to data
-X_2D = model.transform(X_iris)  # 4. transform the data
+test_images = Xtest.reshape(-1, 8, 8)
 
-iris['PCA1'] = X_2D[:, 0]
-iris['PCA2'] = X_2D[:, 1]
-
-# sns.lmplot(x="PCA1", y="PCA2", hue='species', data=iris, fit_reg=False)
-#
-# # Unsupervised Learning Example: Iris Clustering
-#
-from sklearn.mixture import GaussianMixture
-
-model = GaussianMixture(n_components=3, covariance_type='full')  # 2. instantiate model
-model.fit(X_iris)  # 3. fit model to data
-y_gmm = model.predict(X_iris)  # 4. determine labels
-iris['cluster'] = y_gmm
-sns.lmplot(x="PCA1", y="PCA2", data=iris, hue='species',col='cluster', fit_reg=False)
+for i, ax in enumerate(axes.flat):
+    ax.imshow(test_images[i], cmap='binary', interpolation='nearest')
+    ax.text(0.05, 0.05, str(y_model[i]), transform=ax.transAxes,
+            color='green' if (ytest[i] == y_model[i]) else 'red')
 plt.show()
